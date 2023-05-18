e04fdf53b4ee8fd3e5381c5891033bf5c602d3e7
LaZeAsh <ayushrgarg@gmail.com>
2023-05-18 11:34:11 -0700

    Update user model

--- src/database/models/user.ts
+++ src/database/models/user.ts
@@ -1,55 +1,70 @@
 /*
-Purpose of this model: */
+File does not follow the proper naming conventions because of the JAMHacks website...
+(Scott and Andrew do not value proper naming conventions)
+*/
+import { Schema, model } from 'mongoose';
+import mongoose from 'mongoose';
 
-import { Schema, model, Model, Document } from 'mongoose';
-
-interface userInterface extends Document {
-  discordID: string;
-  name: string;
-  email: string;
-  socialMediaInfo: object;
-  isAdmin: boolean;
-  isRegistered: boolean;
-  isAccepted: boolean;
-}
-
-const schema = new Schema<userInterface>(
-  {
-    discordID: {
+const userSchema = new Schema({
+  discord_id: String,
+  name: String,
+  email: String,
+  social_media_info: {
+    linkedin: {
       type: String,
-      required: true
+      default: ''
     },
-    name: {
+    instagram: {
       type: String,
-      required: true
+      default: ''
     },
-    email: {
+    github: {
       type: String,
-      required: true
+      default: ''
     },
-    socialMediaInfo: {
-      linkedin: {
-        type: String
-      },
-      instagram: {
-        type: String
-      }
-    },
-    isAdmin: {
-      type: Boolean,
-      required: true
-    },
-    isRegistered: {
-      type: Boolean,
-      required: true
-    },
-    isAccepted: {
-      type: Boolean,
-      required: true
+    personal_website: {
+      type: String,
+      default: ''
     }
   },
-  { versionKey: false }
-); // Dictates what type of information will be stored and if it's required
+  isAdmin: {
+    type: Boolean,
+    default: false
+  },
+  isRegistered: {
+    type: Boolean,
+    default: false
+  },
+  attendingStatus: {
+    // 0: default, 1: attending, -1: not attending
+    type: Number,
+    default: 0
+  },
+  applicantStatus: {
+    // 0: default, 1: accepted, -1: rejected, 2: waitlisted
+    type: Number,
+    default: 0
+  },
+  applicantRatings: {
+    type: [{ type: mongoose.Types.ObjectId, ref: 'ApplicantRating' }],
+    default: []
+  },
+  signedWaiver: {
+    type: Boolean,
+    default: false
+  },
+  event_specific: { // LIKE WHY IS THIS SNAKE CASE
+    wistem_brunch: {
+      // 0: undecided, 1: confirmed, -1: dismissed
+      type: Number,
+      default: 0
+    },
+    workshopAttendances: { // AND WHY IS THIS CAMEL CASE
+      type: [{ type: mongoose.Types.ObjectId, ref: 'Events' }],
+      default: []
+    }
+  }
+});
 
-const USER: Model<userInterface> = model<userInterface>('user', schema);
+const USER = model('user', userSchema);
 export default USER;
