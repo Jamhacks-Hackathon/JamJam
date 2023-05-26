21f7e2e669692903e183b8c426fa3e84c134b8c2
LaZeAsh <ayushrgarg@gmail.com>
2023-05-25 23:21:11 -0700

    Finish verify command

--- src/commands/verify.ts
+++ src/commands/verify.ts
@@ -3,45 +3,34 @@ import { USER } from '../database';
 export = {
   data: new Discord.SlashCommandBuilder()
     .setName('verify')
-    .setDescription('Verify command!')
-    .addStringOption((option) =>
-      option
-        .setName('email')
-        .setDescription('email you registered with')
-        .setRequired(true)
-    ),
+    .setDescription('Verify command!'),
   async execute(interaction: Discord.CommandInteraction) {
-    const EMAIL = interaction.options.get('email');
-    if (!EMAIL) {
-      return;
-    }
-    console.log(`${EMAIL.value}`);
-    console.log(await USER.exists({ email: EMAIL.value }));
-    console.log(await USER.collection.getIndexes());
-    if (await USER.exists({ email: EMAIL.value })) {
-      await USER.findOne({ email: EMAIL.value }).then(async (doc) => {
-        if (
-          doc?.attendingStatus === 1 &&
-          (doc?.discord_id as string) === `${interaction.user.id}`
-        ) {
-          const ROLE: Discord.Role = (await interaction.guild?.roles.fetch(
-            '1092918980156596224'
-          )) as Discord.Role;
-          const MEMBER: Discord.GuildMember =
-            (await interaction.guild?.members.fetch(
-              `${doc?.discord_id}` as string
-            )) as Discord.GuildMember;
-
-          await MEMBER.roles.add(ROLE);
-          await MEMBER.setNickname(`${doc?.name}`);
-          // Add role and change nickname to real name
-          console.log('cool beans');
+    if (await USER.exists({ discord_id: interaction.user.id })) {
+      await USER.findOne({ discord_id: interaction.user.id }).then(
+        async (doc) => {
+          console.log(doc);
+          if (doc?.attendingStatus === 1) {
+            interaction.reply(`Welcome to JAMHacks 7 ${doc.name}!`);
+            const ROLE: Discord.Role = (await interaction.guild?.roles.fetch(
+              '1092918980156596224'
+            )) as Discord.Role;
+            const MEMBER: Discord.GuildMember =
+              (await interaction.guild?.members.fetch(
+                `${doc?.discord_id}` as string
+              )) as Discord.GuildMember;
+            await MEMBER.roles.add(ROLE);
+            await MEMBER.setNickname(`${doc?.name}`);
+            // Add role and change nickname to real name
+          } else {
+            interaction.reply(
+              `Sorry, you are not confirmed to attend JAMHacks 7! Please contact someone with the Organizer role if you believe that this is a mistake!`
+            );
+          }
         }
-      });
+      );
     } else {
-      // Reply that they arent in the system and to contact jamhacks exec if they think this is a mistake
       await interaction.reply(
-        `Email \`${EMAIL.value}\` is not confirmed to attend the event. If you think this is a mistake please contact one of the JAMHacks organizers with the "Organizer" Role`
+        `Sorry, my database records does not show their ever being a record of <@${interaction.user.id}> registering for JAMHacks 7, contact someone with the Organizer role if you believe that this is a mistake`
       );
     }
   }
